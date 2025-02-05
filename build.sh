a734885d8f34081f2e1e4e98a85d6c0106571e38
Asishsiji <159814550+Asishsiji@users.noreply.github.com>
2025-02-05 18:14:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 -m ensurepip --default-pip
-python3 -m pip install --upgrade pip setuptools==58.2.0 wheel
+python3 -m pip install --upgrade pip setuptools wheel
 python3 -m pip install -r requirements.txt
