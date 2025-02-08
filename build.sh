0b5bf1178009f4ae31e10ed645a433a022799ebd
Asishsiji <159814550+Asishsiji@users.noreply.github.com>
2025-02-08 09:43:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-python3 -m ensurepip --default-pip
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install -r requirements.txt
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+# Create virtual environment
+python -m venv venv
+
+# Activate virtual environment
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    source venv/Scripts/activate  # Windows
+else
+    source venv/bin/activate  # Mac/Linux
+fi
+
+# Upgrade pip, setuptools, and wheel
+python -m pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+python -m pip install -r requirements.txt
+
