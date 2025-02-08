e6eccaa4ae79edf0982c6db31988a6c061ab92e8
Asishsiji <159814550+Asishsiji@users.noreply.github.com>
2025-02-08 09:44:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status.
+set -e  # Stop execution on error
 
-# Create virtual environment
-python -m venv venv
+# Create virtual environment using python3
+python3 -m venv venv
 
 # Activate virtual environment
 if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
@@ -12,8 +12,9 @@ else
 fi
 
 # Upgrade pip, setuptools, and wheel
-python -m pip install --upgrade pip setuptools wheel
+python3 -m pip install --upgrade pip setuptools wheel
 
 # Install dependencies
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+
 
